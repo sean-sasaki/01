db280f8c152dc9201dd42d1dd767debaf212b7e7
sean-sasaki <107918485+sean-sasaki@users.noreply.github.com>
2022-06-21 17:44:44 +0900

    Add files via upload

---
+++ website/js/common.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+$(".openbtn1").click(function () {
+	$(this).toggleClass('active');
+	$("#g-nav").toggleClass('panelactive');
+});
+$("#g-nav a").click(function () {
+	$(".openbtn1").removeClass('active');
+	$("#g-nav").removeClass('panelactive');
+});
